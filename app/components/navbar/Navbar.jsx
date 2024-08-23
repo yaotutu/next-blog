a34b779d5fa5f38ac7d539109aa39574b2df8565
yaotutu <957810668@qq.com>
2024-08-23 19:00:39 +0800

    ✨ (AuthLinks): introduce authentication links component to handle user login/logout   ✨ (authLinks.module.css): add styles for links, burger menu, and responsive menu   ♻️ (Navbar): refactor navbar to include AuthLinks component for better user interaction   ♻️ (layout.tsx): update layout to render Navbar component for consistent navigation

--- app/components/navbar/Navbar.jsx
+++ app/components/navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./navbar.module.css";
 import Image from "next/image";
 import Link from "next/link";
-// import AuthLinks from "../authLinks/AuthLinks";
+import AuthLinks from "../authLinks/AuthLinks";
 // import ThemeToggle from "../themeToggle/ThemeToggle";
 
 const Navbar = () => {
@@ -20,7 +20,7 @@ const Navbar = () => {
         <Link href="/" className={styles.link}>Homepage</Link>
         <Link href="/" className={styles.link}>Contact</Link>
         <Link href="/" className={styles.link}>About</Link>
-        {/* <AuthLinks /> */}
+        <AuthLinks />
       </div>
     </div>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "./components/navbar/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,10 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <div className="container">
-          <div className="wrapper">{children}</div>
+          <div className="wrapper">
+            <Navbar />
+            {children}
+          </div>
         </div>
       </body>
     </html>
