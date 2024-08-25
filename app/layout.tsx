81f362c45075e4b15d748f185e0e58fc665c1de9
yaotutu <957810668@qq.com>
2024-08-25 08:48:56 +0800

    ✨ 新功能(layout.tsx, page.tsx, components)：添加Footer和Menu组件   📝 文档(Menu.jsx, footer.jsx)：增加页脚和菜单的样式及功能   ♻️ 重构(Menu.jsx, menuPosts.jsx)：优化代码结构以提升可读性

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ThemeContextProvider } from "@/context/ThemeContext";
 import ThemeProvider from "@/providers/ThemeProvider";
 import Navbar from "@/components/navbar/Navbar";
+import Footer from "@/components/footer/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -25,6 +26,7 @@ export default function RootLayout({
               <div className="wrapper">
                 <Navbar />
                 {children}
+                <Footer />
               </div>
             </div>
           </ThemeProvider>
--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,17 @@ import Link from "next/link";
 import styles from "./homepage.module.css";
 import Featured from "../components/featured/Featured";
 import CategoryList from "../components/categoryList/CategoryList";
+import Menu from "../components/Menu/Menu";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Featured />
       <CategoryList />
+      <div className={styles.content}>
+        {/* <CardList page={page} /> */}
+        <Menu />
+      </div>
     </div>
   );
 }
