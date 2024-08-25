6f2bf5f2c8a9863c8ec5aa995ea4d46650c2a339
yaotutu <957810668@qq.com>
2024-08-25 17:09:19 +0800

    ✨ 新功能(layout.tsx, AuthProvider.jsx, write/page.jsx, write/writePage.module.css)：添加身份验证提供程序 ✨ 功能(write/page.jsx)：实现写作页面，支持输入标题和描述 🔧 更新(package.json, pnpm-lock.yaml)：增加next-auth和prisma依赖，使身份验证功能可用

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ import { ThemeContextProvider } from "@/context/ThemeContext";
 import ThemeProvider from "@/providers/ThemeProvider";
 import Navbar from "@/components/navbar/Navbar";
 import Footer from "@/components/footer/Footer";
+import AuthProvider from "@/providers/AuthProvider";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -20,17 +22,19 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ThemeContextProvider>
-          <ThemeProvider>
-            <div className="container">
-              <div className="wrapper">
-                <Navbar />
-                {children}
-                <Footer />
+        <AuthProvider>
+          <ThemeContextProvider>
+            <ThemeProvider>
+              <div className="container">
+                <div className="wrapper">
+                  <Navbar />
+                  {children}
+                  <Footer />
+                </div>
               </div>
-            </div>
-          </ThemeProvider>
-        </ThemeContextProvider>
+            </ThemeProvider>
+          </ThemeContextProvider>
+        </AuthProvider>
       </body>
     </html>
   );
