8898571d720f025219806d41549e913f768c0f4b
yaotutu <957810668@qq.com>
2024-08-25 16:56:46 +0800

    ♻️ 重构：重命名组件位置并更新路径引用以提高可维护性 ✨ 新增：引入Prisma作为数据访问层，支持MongoDB ⬆️ 更新：添加react-quill依赖以支持富文本编辑功能 🔧 更新：调整tsconfig设置和依赖配置以匹配新结构

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 import Link from "next/link";
 import styles from "./homepage.module.css";
-import Featured from "../components/featured/Featured";
-import CategoryList from "../components/categoryList/CategoryList";
-import Menu from "../components/Menu/Menu";
+import Featured from "@/components/featured/Featured";
+import CategoryList from "@/components/categoryList/CategoryList";
+import Menu from "@/components/Menu/Menu";
 
 export default function Home() {
   return (
