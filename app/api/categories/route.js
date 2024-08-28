3d9b5460f31fc401b2e0e52798c4425bef0332aa
yaotutu <957810668@qq.com>
2024-08-28 14:54:18 +0800

    ✨ 添加(categories API)：实现GET请求以获取分类列表 🔧 修复(CategoryList组件)：更新fetch请求中的API地址为指定IP地址 💡 更新(CategoryList组件)：启用数据加载功能以渲染分类列表

--- app/api/categories/route.js
+++ app/api/categories/route.js
@@ -1,7 +1,15 @@
+import prisma from "@/utils/connect";
 import { NextResponse } from "next/server";
 
-export const GET = async (req) => {
-  return new NextResponse(JSON.stringify({ message: "GET /api/categories" }), {
-    status: 200,
-  });
+export const GET = async () => {
+  try {
+    const categories = await prisma.category.findMany();
+
+    return new NextResponse(JSON.stringify(categories, { status: 200 }));
+  } catch (err) {
+    console.log(err);
+    return new NextResponse(
+      JSON.stringify({ message: "Something went wrong!" }, { status: 500 })
+    );
+  }
 };
--- app/components/categoryList/CategoryList.jsx
+++ app/components/categoryList/CategoryList.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import Image from "next/image";
 
 const getData = async () => {
-  const res = await fetch("http://localhost:3000/api/categories", {
+  const res = await fetch("http://192.168.55.150:3000/api/categories", {
     cache: "no-store",
   });
 
@@ -16,12 +16,12 @@ const getData = async () => {
 };
 
 const CategoryList = async () => {
-  // const data = await getData(); 
+  const data = await getData();
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Popular Categories</h1>
       <div className={styles.categories}>
-        {[]?.map((item) => (
+        {data?.map((item) => (
           <Link
             href="/blog?cat=style"
             className={`${styles.category} ${styles[item.slug]}`}
