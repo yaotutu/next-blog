7e4449b06af018d59edf0791b9c84a22e3aa76eb
yaotutu <957810668@qq.com>
2024-08-28 17:36:08 +0800

    ♻️ 重构(page.jsx)：恢复解析searchParams.page为整数的逻辑 🔧 更新(dev.db)：保存数据库中对应的更改

--- app/page.jsx
+++ app/page.jsx
@@ -6,8 +6,7 @@ import CardList from "@/components/cardList/CardList";
 import Menu from "@/components/Menu/Menu";
 
 export default function Home({ searchParams }) {
-  // const page = parseInt(searchParams.page) || 1;
-  const page = 1
+  const page = parseInt(searchParams.page) || 1;
 
   return (
     <div className={styles.container}>
