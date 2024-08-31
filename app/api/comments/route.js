257f2dc320929a654054180bda03b9ca1b9a8c3c
yaotutu <957810668@qq.com>
2024-08-31 17:48:00 +0800

    ♻️ 重构(route.js)：更新POST请求以使用auth进行身份验证   🔧 删除未使用的useSession，并清理代码注释

--- app/api/comments/route.js
+++ app/api/comments/route.js
@@ -1,11 +1,9 @@
 import prisma from "@/utils/connect";
-import { useSession } from "next-auth/react";
 import { NextResponse } from "next/server";
-
+import { auth } from "../../../auth";
 // GET ALL COMMENTS OF A POST
 export const GET = async (req) => {
   const { searchParams } = new URL(req.url);
-
   const postSlug = searchParams.get("postSlug");
 
   try {
@@ -26,26 +24,26 @@ export const GET = async (req) => {
 };
 
 // CREATE A COMMENT
-// export const POST = async (req) => {
-//   const session = await useSession();
+export const POST = async (req) => {
+  const session = await auth();
+  console.log("session", session);
+  if (!session) {
+    return new NextResponse(
+      JSON.stringify({ message: "Not Authenticated!" }, { status: 401 })
+    );
+  }
 
-//   if (!session) {
-//     return new NextResponse(
-//       JSON.stringify({ message: "Not Authenticated!" }, { status: 401 })
-//     );
-//   }
+  try {
+    const body = await req.json();
+    const comment = await prisma.comment.create({
+      data: { ...body, userEmail: session.user.email },
+    });
 
-//   try {
-//     const body = await req.json();
-//     const comment = await prisma.comment.create({
-//       data: { ...body, userEmail: session.user.email },
-//     });
-
-//     return new NextResponse(JSON.stringify(comment, { status: 200 }));
-//   } catch (err) {
-//     console.log(err);
-//     return new NextResponse(
-//       JSON.stringify({ message: "Something went wrong!" }, { status: 500 })
-//     );
-//   }
-// };
+    return new NextResponse(JSON.stringify(comment, { status: 200 }));
+  } catch (err) {
+    console.log(err);
+    return new NextResponse(
+      JSON.stringify({ message: "Something went wrong!" }, { status: 500 })
+    );
+  }
+};
