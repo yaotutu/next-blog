228685b0277d63dbb4306c37a0c224bc17ce637c
yaotutu <957810668@qq.com>
2024-09-01 10:14:48 +0800

    ♻️ 重构(CardList.jsx)：将Card组件的key属性从_item._id_改为_item.id_   这样做是为了确保每个组件在列表中的唯一性并提高渲染性能。

--- app/components/cardList/CardList.jsx
+++ app/components/cardList/CardList.jsx
@@ -26,13 +26,12 @@ const CardList = async ({ page, cat }) => {
 
   const hasPrev = POST_PER_PAGE * (page - 1) > 0;
   const hasNext = POST_PER_PAGE * (page - 1) + POST_PER_PAGE < count;
-
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Recent Posts</h1>
       <div className={styles.posts}>
         {posts?.map((item) => (
-          <Card item={item} key={item._id} />
+          <Card item={item} key={item.id} />
         ))}
       </div>
       <Pagination page={page} hasPrev={hasPrev} hasNext={hasNext} />
