a312ba12210f7559da72caa5566f15bd748fa664
yaotutu <957810668@qq.com>
2024-09-02 09:18:23 +0800

    ♻️ 重构(Navbar.jsx, navbar.module.css)：移除CSS样式并使用Tailwind CSS类   ✨ 功能(Navbar.jsx)：优化导航栏布局与样式，提升响应式设计

--- app/components/navbar/Navbar.jsx
+++ app/components/navbar/Navbar.jsx
@@ -7,19 +7,19 @@ import ThemeToggle from "../themeToggle/ThemeToggle";
 
 const Navbar = () => {
   return (
-    <div className={styles.container}>
-      <div className={styles.social}>
+    <div className="flex items-center justify-between h-[100px]">
+      <div className="gap-[10px] flex-1 lg:text-[32px] md:text-[24px] lg:text-left  hidden lg:flex">
         <Image src="/facebook.png" alt="facebook" width={24} height={24} />
         <Image src="/instagram.png" alt="instagram" width={24} height={24} />
         <Image src="/tiktok.png" alt="tiktok" width={24} height={24} />
         <Image src="/youtube.png" alt="youtube" width={24} height={24} />
       </div>
-      <div className={styles.logo}>垚土土</div>
-      <div className={styles.links}>
+      <div className={"flex-1 text-center text-[36px] font-bold"}>垚土土</div>
+      <div className={"flex items-center gap-[20px] flex-1 text-[20px] xl:text-[18px] xl:gap-[15px] sm:justify-end"}>
         <ThemeToggle />
-        <Link href="/" className={styles.link}>Homepage</Link>
-        <Link href="/" className={styles.link}>Contact</Link>
-        <Link href="/" className={styles.link}>About</Link>
+        <Link href="/" className={"hidden sm:inline"}>Homepage</Link>
+        <Link href="/" className={"hidden sm:inline"}>Contact</Link>
+        <Link href="/" className={"hidden sm:inline"}>About</Link>
         <AuthLinks />
       </div>
     </div>
