8e0ce0f39177b34f7ac1b34cbd075d45118c290c
yaotutu <957810668@qq.com>
2024-09-07 17:05:57 +0800

    ✨ 引入功能(帖子路由)：实现基于身份验证的POST请求 💡 增加注释(WritePage.jsx)：详细说明slugify中文标题的功能 ⬆️ 升级依赖：添加pinyin和segmentit库以支持中文slug生成 ♻️ 重构(WritePage.jsx)：改善Markdown编辑器的插件管理和处理逻辑

--- app/api/posts/route.js
+++ app/api/posts/route.js
@@ -1,6 +1,6 @@
-// import { getAuthSession } from "@/utils/auth";
 import prisma from "@/utils/connect";
 import { NextResponse } from "next/server";
+import { auth } from "../../../auth";
 
 export const GET = async (req) => {
   const { searchParams } = new URL(req.url);
@@ -33,26 +33,26 @@ export const GET = async (req) => {
 };
 
 // CREATE A POST
-// export const POST = async (req) => {
-//   const session = await getAuthSession();
+export const POST = async (req) => {
+  const session = await auth();
 
-//   if (!session) {
-//     return new NextResponse(
-//       JSON.stringify({ message: "Not Authenticated!" }, { status: 401 })
-//     );
-//   }
+  if (!session) {
+    return new NextResponse(
+      JSON.stringify({ message: "Not Authenticated!" }, { status: 401 })
+    );
+  }
 
-//   try {
-//     const body = await req.json();
-//     const post = await prisma.post.create({
-//       data: { ...body, userEmail: session.user.email },
-//     });
+  try {
+    const body = await req.json();
+    const post = await prisma.post.create({
+      data: { ...body, userEmail: session.user.email },
+    });
 
-//     return new NextResponse(JSON.stringify(post, { status: 200 }));
-//   } catch (err) {
-//     console.log(err);
-//     return new NextResponse(
-//       JSON.stringify({ message: "Something went wrong!" }, { status: 500 })
-//     );
-//   }
-// };
+    return new NextResponse(JSON.stringify(post, { status: 200 }));
+  } catch (err) {
+    console.log(err);
+    return new NextResponse(
+      JSON.stringify({ message: "Something went wrong!" }, { status: 500 })
+    );
+  }
+};
--- app/write/page.jsx
+++ app/write/page.jsx
@@ -1,29 +1,46 @@
 "use client";
 
-import Image from "next/image";
-import styles from "./writePage.module.scss";
-import { useEffect, useState } from "react";
-import "react-quill/dist/quill.bubble.css";
-import { useRouter } from "next/navigation";
+import MarkdownIt from "markdown-it";
 import { useSession } from "next-auth/react";
-import ReactQuill from "react-quill";
-import MarkdownIt from 'markdown-it';
-import MdEditor from 'react-markdown-editor-lite';
-import 'react-markdown-editor-lite/lib/index.css';
+import dynamic from "next/dynamic";
+import { useRouter } from "next/navigation";
+import pinyin from "pinyin";
+import { useState } from "react";
+import "react-markdown-editor-lite/lib/index.css";
+import "react-quill/dist/quill.bubble.css";
+import styles from "./writePage.module.scss";
 const mdParser = new MarkdownIt(/* Markdown-it options */);
 
-// Finish!
-function handleEditorChange({ html, text }) {
-  console.log('handleEditorChange', html, text);
-}
+const plugins = [
+  "header",
+  "font-bold",
+  "font-italic",
+  "font-underline",
+  "font-strikethrough",
+  "list-unordered",
+  "list-ordered",
+  "block-quote",
+  "block-wrap",
+  "block-code-inline",
+  "block-code-block",
+  "table",
+  "link",
+  "clear",
+  "logger",
+  "mode-toggle",
+  "full-screen",
+  "tab-insert",
+];
+const MdEditor = dynamic(() => import("react-markdown-editor-lite"), {
+  ssr: false,
+});
+
 const WritePage = () => {
   const { status } = useSession();
   const router = useRouter();
   const [value, setValue] = useState("");
   const [title, setTitle] = useState("");
   const [catSlug, setCatSlug] = useState("");
-
-
 
   if (status === "loading") {
     return <div className={styles.loading}>Loading...</div>;
@@ -33,7 +50,7 @@ const WritePage = () => {
   //   router.push("/");
   // }
 
-  const slugify = (str) =>
+  const slugifyEnglish = (str) =>
     str
       .toLowerCase()
       .trim()
@@ -41,16 +58,35 @@ const WritePage = () => {
       .replace(/[\s_-]+/g, "-")
       .replace(/^-+|-+$/g, "");
 
+  const slugifyChinese = (title) => {
+    // 将中文转换为拼音
+    const pinyinTitle = pinyin(title, {
+      style: pinyin.STYLE_NORMAL, // 使用普通拼音风格（不带声调）
+      heteronym: false, // 仅返回一个拼音
+    })
+      .flat()
+      .join(" "); // 将拼音数组转化为字符串
+
+    // 使用之前的 slugify 逻辑生成 slug
+    return pinyinTitle
+      .toLowerCase()
+      .trim()
+      .replace(/[^\w\s-]/g, "") // 移除非字母、数字、空格和连字符的字符
+      .replace(/[\s_-]+/g, "-") // 替换空格和下划线为连字符
+      .replace(/^-+|-+$/g, ""); // 移除开头或结尾的连字符
+  };
+
   const handleSubmit = async () => {
+    const bodyData = {
+      title,
+      desc: value,
+      img: "",
+      slug: slugifyChinese(title),
+      catSlug: catSlug || "style", //If not selected, choose the general category
+    };
     const res = await fetch("/api/posts", {
       method: "POST",
-      body: JSON.stringify({
-        title,
-        desc: value,
-        img: media,
-        slug: slugify(title),
-        catSlug: catSlug || "style", //If not selected, choose the general category
-      }),
+      body: JSON.stringify(bodyData),
     });
 
     if (res.status === 200) {
@@ -59,6 +95,9 @@ const WritePage = () => {
     }
   };
 
+  const handleEditorChange = ({ html, text }) => {
+    setValue(html);
+  };
   return (
     <div className={styles.container}>
       <input
@@ -68,7 +107,14 @@ const WritePage = () => {
         onChange={(e) => setTitle(e.target.value)}
       />
       <div className={styles.editor}>
-            <MdEditor renderHTML={text => mdParser.render(text)} onChange={handleEditorChange} />
+        <MdEditor
+          onChange={handleEditorChange}
+          renderHTML={(text) => mdParser.render(text)}
+          plugins={plugins}
+          style={{
+            width: "100%",
+          }}
+        />
       </div>
       <button className={styles.publish} onClick={handleSubmit}>
         Publish
